89d5d71af073d0d8edbf3fdde7ce0954fdeb8696
Quang L <lvquang88@gmail.com>
2023-10-02 01:03:38 +0700

    Update github action ci (#14)
    * Test (#7)
    
    * Update test 3 (#8)
    
    * Feature #4: Test
    
    * Test (#7)
    
    * Update test 3 (#8)
    
    * Feature #4: Test
    
    * Update ci

--- test.js
+++ test.js
@@ -3,10 +3,12 @@ const BlogPost = require('./models/blogpost');
 const { ObjectId } = require('bson');
 
 
-mongoose.connect('mongodb://localhost/test_my_database', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/test_my_database', { useNewUrlParser: true });
 
 // Test 1
 // Test 2
+// Test 3
+// Test 4
 
 // BlogPost.create({
 //     title: 'MƯA THÁNG SÁU',
